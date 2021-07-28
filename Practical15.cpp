2972f4b451aa88a2cee7617505cf854088205460
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-07-28 14:20:36 +0530

    Practical 15

---
+++ Practical15.cpp
@@ -0,0 +1,47 @@
+/*
+Program 15:
+
+Write a Program to store a function (polynomial/exponential), and then evaluate the polynomial, 
+(For example store f (x) = 4n3 + 2n + 9 in an array and for a given value of n, say n = 5, evaluate (i.e. compute the value of f(5)).
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int arr[20], deg, x, sum = 0;
+    char ch;
+    do
+    {
+        cout << "Enter the degree of the polynomial :";
+        cin >> deg;
+        for (int i = deg; i >= 0; i--)
+        {
+            cout << "Enter the coefficient of degree " << i << " :";
+            cin >> arr[i];
+        }
+        cout << "Our required polynomial is :";
+        cout << arr[deg] << "x^" << deg;
+        for (int i = deg - 1; i > 0; i--)
+        {
+            if (arr[i] > 0)
+                cout << "+" << arr[i] << "x^" << i;
+            else
+                cout << "-" << arr[i] << "x^" << i;
+        }
+        cout << "+" << arr[0] << "x^" << 0;
+        cout << "\n Enter the value of x : ";
+        cin >> x;
+        for (int i = deg; i >= 0; i--)
+        {
+            sum += (arr[i] * pow(x, i));
+        }
+        cout << "\nThe solution of this polynomial is :" << sum;
+        char ch;
+        cout << "\n Do you want to continue?(y/n):";
+        cin >> ch;
+
+    } while (ch == 'y' || ch == 'Y');
+}
